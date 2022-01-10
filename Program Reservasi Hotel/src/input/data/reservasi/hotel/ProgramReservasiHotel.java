17155edf59dd3fba8cb5b98ec5c7b5abdb74bf52
Nurul1903 <75475462+Nurul1903@users.noreply.github.com>
2022-01-10 22:25:49 +0800

    UAS PROJECT

---
+++ Program Reservasi Hotel/src/input/data/reservasi/hotel/ProgramReservasiHotel.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package input.data.reservasi.hotel;
+
+/**
+ *
+ * @author Ardy Sendleep
+ */
+public class ProgramReservasiHotel {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        Frame_program_reservasi_hotel Laund = new Frame_program_reservasi_hotel();
+        Laund.setVisible(true);
+    }
+}
